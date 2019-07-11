3a8e4bce11e36ac8ad213866c710681083d9c91b
Tara Roshan <tara.roshan12@gmail.com>
2019-07-11 17:29:41 -0500

    Update data_code.R
    joined district and unique.all (Grade_7 and grade_K) into match. Still need to try and find a way to see if there are any school names that appear in only one of the sets and not the other but share a middle_6 code. Then we need to start doing ggplot work with the data and create a SBM using an R package.

--- data_code.R
+++ data_code.R
@@ -9,21 +9,28 @@ colnames(grade_K)[1]<-"fac.id"
 inner_join(grade_K,Grade_7, by="fac.id")-> all
 
 
-#look for how often middle four #s appear and see unique middle_4 combos
+#create middle_6 column and find unique occurrances -> unique.all
 all%>%mutate(middle_4 = str_sub(fac.id, 4, 7),middle_6 = as.integer(str_sub(fac.id, 2, 7)))->all
 code.freq<-table(all$middle_6)
 names(code.freq)[code.freq==1]->code1.freq
-
 all%>%filter(middle_4 %in% as.numeric(code1.freq))->unique.all
 
-#read TEA csv file. all has middle_4 because of grade_K
+#read TEA csv file and join with unique.all by middle_6 values
 district <- read.csv("district.csv")
 district%>%rename(c("middle_4"="middle_6"))->district
 inner_join(unique.all,district,by="middle_6")->match
+
+#renaming column and making its values have class of character
+match <- rename(match, c('Facility Name' = 'Facility_Name'))
+Facility.Name=as.character(Facility.Name)
 View(match)
 
+#trying to test that the names match between district and unique.all in match
+match%>%merge(as.character(Facility.Name),as.character(Facility_Name))
+code.freq<-table(all$middle_6)
+names(code.freq)[code.freq==1]->code1.freq
 
-
+#to do list:
 #check middle_4 and school names match
 #look at correlation between CVE and the schools, look for data more specialized than just county
 #other set has how manny people took test, which can be used for population
